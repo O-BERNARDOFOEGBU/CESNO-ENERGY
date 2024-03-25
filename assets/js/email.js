4c17aeb3d00345d22f037c9e70bbcb46e2e06a1e
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-03-25 19:03:30 +0100

    gitignore

---
+++ assets/js/email.js
@@ -0,0 +1,27 @@
+import { SERVICE_ID, TEMPLATE_ID } from "../js/keys";
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  // const serviceID = "service_vifytjq";
+  // const templateID = "template_nbzwh1g";
+  const serviceID = SERVICE_ID;
+  const templateID = TEMPLATE_ID;
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
